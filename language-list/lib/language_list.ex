7025d0c415b924a43936d7aa39b138ae9b65cc98
vishal <vishal.auroville@gmail.com>
2023-03-27 15:46:33 +0530

    first_commit

--- language-list/lib/language_list.ex
+++ language-list/lib/language_list.ex
@@ -3,23 +3,22 @@ defmodule LanguageList do
     []
   end
 
-  def add(list, language) do
-    # Please implement the add/2 function
+  def add(list, lang) do
+    [lang | list]
   end
-
   def remove(list) do
-    # Please implement the remove/1 function
+    tl(list)
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    hd(list)
   end
 
   def count(list) do
-    # Please implement the count/1 function
+    length(list)
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    "Elixir" in list
   end
 end
--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,26 +1,21 @@
 defmodule Lasagna do
 
-  # Please define the 'expected_minutes_in_oven/0' function
   def expected_minutes_in_oven do
     40
   end
 
-  # Please define the 'remaining_minutes_in_oven/1' function
   def remaining_minutes_in_oven(x) do
     expected_minutes_in_oven() - x
   end
 
-  # Please define the 'preparation_time_in_minutes/1' function
   def preparation_time_in_minutes(number_of_layer) do
     number_of_layer * 2
   end
 
-  # Please define the 'total_time_in_minutes/2' function
  def total_time_in_minutes(number_of_layer, number_of_minutes_the_lasagna_has_been_in_the_oven) do
   (number_of_layer * 2) + number_of_minutes_the_lasagna_has_been_in_the_oven
  end
 
-  # Please define the 'alarm/0' function
   def alarm do
     "Ding!"
   end
