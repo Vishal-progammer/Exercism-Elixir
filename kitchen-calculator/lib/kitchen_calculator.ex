c4da04f3329ab731e443cd83b125cd9cc530c179
vishal <vishal.auroville@gmail.com>
2023-04-03 09:39:02 +0530

    kitcken calculator

---
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,16 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair) do
+  end
+
+  def to_milliliter(volume_pair) do
+    # Please implement the to_milliliter/1 functions
+  end
+
+  def from_milliliter(volume_pair, unit) do
+    # Please implement the from_milliliter/2 functions
+  end
+
+  def convert(volume_pair, unit) do
+    # Please implement the convert/2 function
+  end
+end
